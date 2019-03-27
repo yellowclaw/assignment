8799dc99711d98db61a8e04ef86943054a39f34c
Yaroslav Voloshyn <yaroslav.voloshyn@sombrainc.com>
2019-03-27 18:23:38 +0200

    Added pre-commit hook. Changed server structure.

--- client/src/app/opportunities/opportunities.service.ts
+++ client/src/app/opportunities/opportunities.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/index';
 import { Opportunity } from './opportunities.component';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
